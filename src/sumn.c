85b3ce357761e6909e84bb6e630bdae9947eacfd
au706600 <202106601@post.au.dk>
2021-11-22 22:44:05 +0100

    implemented sumn.c

--- src/sumn.c
+++ src/sumn.c
@@ -1,8 +1,17 @@
 #include "sumn.h"
 
 #include<stdio.h>
+#include <assert.h>
 
 int sumn (int n)
 {
-    return 0;
+    /* precondition */
+assert (n >=1); 
+
+/* postcondition */
+if (n==1){
+return 1; 
+}else {
+return (2*1-1) + (2*2-1) + (2*n-1); 
+}
 }
